32777e9801b8c01c160635917c688990f6997f79
yuuta-kun6784 <186603347+yuuta-kun6784@users.noreply.github.com>
2025-03-31 12:31:51 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Providers;
+use Illuminate\Support\Facades\URL;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -20,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
+        }
     }
 }
