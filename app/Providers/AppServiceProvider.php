29293fb40325d424da41ff2d6fcfb7847f5f261a
yuuta-kun6784 <186603347+yuuta-kun6784@users.noreply.github.com>
2025-03-31 12:47:36 +0900

    https化

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        if (env('APP_ENV') === 'production') {
+        if (\App::environment(['production'])) {
             URL::forceScheme('https');
         }
     }
